1b12dad22545d09b34f562366a0bbe2b64179d7b
phm1605 <phm1605@gmail.com>
2025-06-30 14:16:37 +0700

    updates

--- chapter8/CollisionManager.h
+++ chapter8/CollisionManager.h
@@ -1,2 +1,9 @@
+#pragma once 
 
-
+class CollisionManager {
+public:
+    void checkPlayerEnemyBulletCollision(Player* pPlayer);
+    void checkPlayerEnemyCollision(Player* pPlayer, const std::vector<GameObject*> &objects);
+    void checkEnemyPlayerBulletCollision(const std::vector<GameObject*> &objects);
+    void checkPlayerTileCollision(Player* pPlayer, const std::vector<TileLayer*> &collisionLayers);
+};
